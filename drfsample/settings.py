35b214e18c9ff102e9f0a38572ac6bab069a9469
nuclearpinguin <turbaszek@gmail.com>
2019-04-15 20:55:13 +0200

    build: dockerize it!

--- drfsample/settings.py
+++ drfsample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0m$fi%hincjkl&8h+@x%^nd4h@qpz3h)la33a5m$&!-b5@c!m0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
